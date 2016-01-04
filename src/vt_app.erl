a130f389a2da44c2f41d0033a593fd39303161f6
Kyrylo Silin <silin@kyrylo.org>
2016-01-04 14:28:57 +0200

    Depend on Cowboy and implement basic Web and Websocket servers

--- src/vt_app.erl
+++ src/vt_app.erl
@@ -16,6 +16,15 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile(
+                 [{'_',
+                   [
+                    {"/", vt_web_handler, []},
+                    {"/ws", vt_ws_handler, []}
+                   ]
+                  }]),
+    {ok, _} = cowboy:start_http(http_listener, 100, [{port, 7332}],
+                                [{env, [{dispatch, Dispatch}]}]),
     vt_sup:start_link().
 
 %%--------------------------------------------------------------------
